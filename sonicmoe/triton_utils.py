9edb41b3b78840bba416c13c3623d5577be994e4
SigureMo <sigure.qaq@gmail.com>
2026-01-07 21:06:35 +0800

    remove scope on wrap_triton_kernel

--- sonicmoe/triton_utils.py
+++ sonicmoe/triton_utils.py
@@ -6,5 +6,5 @@ def wrap_triton_kernel(triton_kernel):
             self.kernel = kernel
 
         def __getitem__(self, index):
-            return paddle.use_compat_guard(enable=True, scope={"triton"})(self.kernel[index])
+            return paddle.use_compat_guard(enable=True)(self.kernel[index])
     return WrappedTritonKernel(triton_kernel)
