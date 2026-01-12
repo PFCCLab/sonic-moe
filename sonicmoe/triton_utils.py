765ed540d3fa18a03083574fb17b2a0c8efe7080
SigureMo <sigure.qaq@gmail.com>
2026-01-12 14:36:48 +0800

    update triton utils to use a lite guard

--- sonicmoe/triton_utils.py
+++ sonicmoe/triton_utils.py
@@ -1,4 +1,19 @@
 import paddle
+import sys
+
+def swap_torch_guard(fn):
+    def wrapped_fn(*args, **kwargs):
+        if "torch" not in sys.modules:
+            return fn(*args, **kwargs)
+        torch_module = sys.modules["torch"]
+        sys.modules["torch"] = paddle
+        try:
+            return fn(*args, **kwargs)
+        finally:
+            sys.modules["torch"] = torch_module
+
+    return wrapped_fn
+
 
 def wrap_triton_kernel(triton_kernel):
     class WrappedTritonKernel:
@@ -6,5 +21,9 @@ def wrap_triton_kernel(triton_kernel):
             self.kernel = kernel
 
         def __getitem__(self, index):
-            return paddle.use_compat_guard(enable=True, silent=True)(self.kernel[index])
+            return swap_torch_guard(self.kernel[index])
+
+        def __getattr__(self, name):
+            return getattr(self.kernel, name)
+
     return WrappedTritonKernel(triton_kernel)
